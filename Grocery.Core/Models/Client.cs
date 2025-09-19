57710a297e14fff1d2d8f13faee8fea35f606eb1
Wouter Winkelman <s1181451@student.windesheim.nl>
2025-09-19 10:22:34 +0200

    Added needed changes to Grocery.App/MauiProgram.cs and Grocery.Core/Models/Client.cs
    Removed // for better functionality

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -1,14 +1,17 @@
-﻿
-namespace Grocery.Core.Models
+﻿namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        private string _emailAddress;
-        private string _password { get; set; }
-        public Client(int id, string name, string emailAddress, string password) : base(id, name)
+        public string EmailAddress { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public Client() : base(0, string.Empty) { }
+
+        public Client(int id, string name, string emailAddress, string password)
+            : base(id, name)
         {
-            _emailAddress=emailAddress;
-            _password=password;
+            EmailAddress = emailAddress;
+            Password = password;
         }
     }
 }
